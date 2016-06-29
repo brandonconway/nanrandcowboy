f6cb9474479bf802a392b1010081d7e65bda1082
Brandon Conway <conwayb@pdx.edu>
2016-06-28 23:27:03 -0700

    Slight cleanup of front.js
    This needs refactor into a proper module.

--- main/htdocs/static/js/front.js
+++ main/htdocs/static/js/front.js
@@ -10,20 +10,30 @@ var cury = 200;
 var xdirection = 1;
 var ydirection = 1;
 var mod = 0;
+var max_array = 2000;
 
-function draw(e) {
-    mod = mod+1;
-    if(mod%29 == 0){
-    xdirection = Math.random() < 0.5 ? 1 : -1;
-    ydirection = Math.random() < 0.4 ? 1 : -1;
-    }
-  if (curx > 900)  xdirection = -1;
-  if (curx < 60)  xdirection = 1;
+function draw () {
+  mod = mod+1;
+  
+  if (points.length > max_array) {
+     console.log(points);
+     points.length = 0;
+     points.push({x: curx, y: cury});
+     console.log(points);
+  }
+
+  if (mod % 29 == 0) {
+      xdirection = Math.random() < 0.5 ? 1 : -1;
+      ydirection = Math.random() < 0.4 ? 1 : -1;
+  }
+
+  if (curx > 900) xdirection = -1;
+  if (curx < 60) xdirection = 1;
   if (cury > 425 ) ydirection = -1;
   if (cury < 100 ) ydirection = 1;
+
   rand_x = (Math.floor(Math.random() * 9) + 1) * xdirection;
   rand_y = (Math.floor(Math.random() * 9) + 1) * ydirection;
-
 
   r = Math.floor(Math.random() * 220) + 1;
   g = Math.floor(Math.random() * 240) + 1;
@@ -32,7 +42,7 @@ function draw(e) {
   curx = curx + rand_x;
   cury = cury + rand_y;
   points.push({ x: curx, y: cury });
-  //ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
   ctx.beginPath();
   ctx.moveTo(points[points.length - 2].x, points[points.length - 2].y);
   ctx.lineTo(points[points.length - 1].x, points[points.length - 1].y);
@@ -46,17 +56,19 @@ function draw(e) {
     if (d < area) {
       ctx.beginPath();
       ctx.strokeStyle = 'rgba('+r+','+g+','+b+',0.3)';
-      ctx.moveTo( points[points.length-1].x + (dx * 0.2), points[points.length-1].y + (dy * 0.2));
-      ctx.lineTo( points[i].x - (dx * 0.2), points[i].y - (dy * 0.2));
+      ctx.moveTo( points[points.length-1].x + (dx * 0.22), points[points.length-1].y + (dy * 0.22));
+      ctx.lineTo( points[i].x - (dx * 0.22), points[i].y - (dy * 0.22));
       ctx.stroke();
     }
   }
 };
+
 function clear(){
   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 }
-$(document).ready(function(e){
+
+$(document).ready(function () {
   points.push({ x: curx, y: cury });
-    var d = window.setInterval(function(){draw(e);}, 50);
+  var d = window.setInterval(function () { draw();}, 50);
 });
 
